0e8652e189f0a188b0d51afb1030b651799be93d
ronyonka <rononyonka@gmail.com>
2025-01-30 16:43:48 +0300

    changed login url

--- client/src/components/LoginScreen.js
+++ client/src/components/LoginScreen.js
@@ -6,7 +6,7 @@ const { colors, fontSizes } = theme;
 const LOGIN_URI =
   process.env.NODE_ENV !== 'production'
     ? 'http://localhost:8888/login'
-    : 'https://spotify-profile.herokuapp.com/login';
+    : 'https://ronyonka.github.io/spotify-profile/login';
 
 const Login = styled(Main)`
   ${mixins.flexCenter};
