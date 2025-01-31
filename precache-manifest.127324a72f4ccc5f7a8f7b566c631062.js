3ee89f13e37684353c9f5bea055f229caa7e9b18
ronyonka <rononyonka@gmail.com>
2025-01-31 14:34:42 +0300

    Updates

---
+++ precache-manifest.127324a72f4ccc5f7a8f7b566c631062.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ca5cde13c80728b0b18a288397a448",
+    "url": "/spotify-profile/index.html"
+  },
+  {
+    "revision": "a64d9545b20cd6db6cda",
+    "url": "/spotify-profile/static/js/2.6622e0e6.chunk.js"
+  },
+  {
+    "revision": "4c373b4a3e80cd53a949",
+    "url": "/spotify-profile/static/js/main.e54f480b.chunk.js"
+  },
+  {
+    "revision": "0ff6994666c8486b09eb",
+    "url": "/spotify-profile/static/js/runtime~main.83a91619.js"
+  }
+]);
